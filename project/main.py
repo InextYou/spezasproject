c933c78f573493bccbf9aa8a32de7e73d42529f7
Fabian Erlenbusch <fabian.erlenbusch@freenet.de>
2019-12-10 12:38:12 +0100

    environment-setup

---
+++ project/main.py
@@ -0,0 +1,4 @@
+from project.ml_pipeline import MLPipeline
+
+if __name__ == "__main__":
+    MLPipeline()
---
+++ project/ml_pipeline.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+
+class MLPipeline:
+
+    def __init__(self):
+        pass
+
+    def load_data(self):
+
