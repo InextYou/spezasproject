fixed: min max plots for each column fixed: bins for histograms todo: boxplot scaling fail

project/datanalysis.py
@@ -10,13 +10,14 @@ print(df.describe())
 
 headers = list(df)
 print(headers)
-for header in headers:
+'''for header in headers:
     if header != 'class':
         print('----------'+header+'----------')
         print(df[header].describe())
-        df[header].hist(bins=df[header].size) ##macht des sinn? 100 oder 1000 einträge im Histogramm bei 28k insgesamt isch ja ned die ganze wahrheit
+        df[header].hist(bins=100) ##macht des sinn? 100 oder 1000 einträge im Histogramm bei 28k insgesamt isch ja ned die ganze wahrheit
         plt.show() ##todo: scaling von der axe isch manchmal verschickt
 
+#geht ned mit zu viele bins, schmiert komplett ab 
 
 
 ##to do: plot min and max values of all cols to show distribution
@@ -26,10 +27,15 @@ plt.show()
 df.min().plot()
 plt.show()
 
-df.max.plot()
-df.min.plot()
+df.max().plot()
+df.min().plot()
 plt.show()
-#todo: 0 y achse nach oben verschieben, scaling andersch dass 0.xxx werte auch angezeigt werden?
+'''
+data_array = np.array(df.iloc[:,0:18])
+
+bp = plt.boxplot(data_array,patch_artist=True, notch=True)
+
+plt.show()
 
 
 